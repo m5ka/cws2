66771d030f265a58f1b957b8d5b6e947d0f59925
Marzena <git@marzka.cafe>
2023-03-29 10:33:39 +0100

    🪲 fix bug in user last seen middleware

--- cws2/middleware.py
+++ cws2/middleware.py
@@ -7,16 +7,24 @@ class LogUserLastSeenMiddleware:
             "REMOTE_ADDR"
         )
 
+    def get_route(self, request):
+        try:
+            return request.resolver_match.url_name[:64]
+        except AttributeError:
+            return None
+
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
         if hasattr(request, "user") and request.user.is_authenticated:
-            request.user.last_seen_ip = self.get_ip_address(request)
-            request.user.last_seen_route = request.resolver_match.url_name[:64]
-            request.user.last_seen_at = timezone.now()
-            request.user.save(
-                update_fields=["last_seen_ip", "last_seen_route", "last_seen_at"]
-            )
+            route = self.get_route(request)
+            if route:
+                request.user.last_seen_ip = self.get_ip_address(request)
+                request.user.last_seen_route = route
+                request.user.last_seen_at = timezone.now()
+                request.user.save(
+                    update_fields=["last_seen_ip", "last_seen_route", "last_seen_at"]
+                )
         return response
