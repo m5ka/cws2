6fa850e2a2e1aa491774792aea73cadeea64857c
Maria <25516941+m5ka@users.noreply.github.com>
2023-10-10 15:01:23 +0100

    🔤 import dialect model in model module

--- cws2/models/__init__.py
+++ cws2/models/__init__.py
@@ -1,10 +1,11 @@
-from cws2.models.language import Language
+from cws2.models.language import Dialect, Language
 from cws2.models.permission import GroupPermission, UserPermission
 from cws2.models.phono_system import Phone, PhonoSystem
 from cws2.models.user import Group, GroupMembership, User, UserProfile
 
 __all__ = [
     # language
+    "Dialect",
     "Language",
     # permission
     "GroupPermission",
