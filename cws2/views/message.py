82ffb45e27661bfbffc3a61a5375b7538dd63f84
m5ka <m5ka@posteo.de>
2024-03-28 16:40:11 +0000

    ✉️ show messages in message view

--- cws2/views/message.py
+++ cws2/views/message.py
@@ -5,3 +5,12 @@ from cws2.views.base import View
 class IndexMessageView(View):
     template_name = "cws2/message/index.jinja"
     body_colour = "orange"
+
+    def get_context_data(self, **kwargs):
+        return {
+            **super().get_context_data(**kwargs),
+            "messages": (
+                Message.objects.filter(recipient=self.request.user)
+                .order_by("recipient", "-created_at").distinct("recipient")
+            ),
+        }
